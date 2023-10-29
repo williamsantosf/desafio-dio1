5a02ba415772929a6b9c0a3fdd5657f71d71b56f
WILLIAM <william.santosf@gmail.com>
2023-10-29 02:11:01 -0300

    Desafio Felipao

---
+++ index.js
@@ -0,0 +1,32 @@
+let nomeHeroi = "Lanterna Verde";
+let xpHeroi = 5000;
+
+
+switch (true) {
+    case xpHeroi < 1000:
+      console.log("O Herói de nome " + nomeHeroi + " está no nível Ferro");
+      break;
+    case xpHeroi >= 1001 && xpHeroi <= 2000:
+      console.log("O Herói de nome " + nomeHeroi + " está no nível Bronze");
+      break;
+    case xpHeroi >= 2001 && xpHeroi <= 5000:
+      console.log("O Herói de nome " + nomeHeroi + " está no nível Prata");
+      break;
+    case xpHeroi >= 6001 && xpHeroi <= 7000:
+      console.log("O Herói de nome " + nomeHeroi + " está no nível Ouro");
+      break;
+    case xpHeroi >= 7001 && xpHeroi <= 8000:
+      console.log("O Herói de nome " + nomeHeroi + " está no nível Platina");
+      break;
+    case xpHeroi >= 8001 && xpHeroi <= 9000:
+      console.log("O Herói de nome " + nomeHeroi + " está no nível Ascendente");
+      break;
+    case xpHeroi >= 9001 && xpHeroi <= 10000:
+      console.log("O Herói de nome " + nomeHeroi + "está no nível Imortal");
+      break;
+    case xpHeroi >= 10001:
+      console.log("O Herói de nome " + nomeHeroi + " está no nível Radiante");
+      break;
+  }
+  
+  //Não observei a necessidade de utilizar um laço de repetição para chegar no objetivo.
